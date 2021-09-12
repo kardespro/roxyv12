8a05a548fa1845ecd8e356526923aca372671f45
kardespro <61694826+kardespro@users.noreply.github.com>
2021-09-12 12:54:51 +0400

    Create roxyapi.js

---
+++ roxyapi.js
@@ -0,0 +1,63 @@
+const requ = require("request");
+const db = require("quick.db");
+const Discord = require("discord.js") 
+const ayar = require("./server.js")
+const nico = new Discord.Client() 
+class greesy {
+  constructor(key){
+    this.greesyAuth = key;
+    return key;
+  }
+  static config(key){
+    if(key == "key"){
+   return  this.greesyAuth;
+      }
+  }
+  static res(){
+    return "Bot Yeniden Başlatılıyor..... " 
+    process.exit(0);
+    setTimeout(function(){ 
+      return "Bot Yeniden başaltıldı! " 
+         }, 
+               5000);
+   } 
+  static promoKod(kod){
+    if(!kod) return "Kod Belirt";
+    var result = requ(`https://hyper-gusty-node.glitch.me/api/v1/promocode/check/${kod}/`);
+    return result;
+    }
+  static premiumCheck(kullid){
+    if(!kullid) return new TypeError("Kullanici id belirtmedin");
+    var resDB = db.fetch(`abonelik_${kullid}`);
+    return resDB;
+    }
+  static duyuruekle(name) {
+    if(!name) return new TypeError("Bir Metin Gir! ") 
+    db.set(`duyuru`, name) 
+    return `Eklendi! Önizleme: ${name} `
+   } 
+  static fetchUser(kid){
+    if(!kid) return new TypeError("Kullanıcı ID'si Belirt! ") 
+    var te = nico.users.cache.get(kid)
+    return te;
+   } 
+  static bakimCheck(){
+    var resBakim = db.fetch(`bakim`);
+    return resBakim;
+    }
+  static bakim(tool){
+    if(!tool) return new TypeError("ac / kapat Yaz");
+    if(tool == "ac"){
+      db.set(`bakim`,true);
+      nico.user.setActivity("Maintance Mode ON")
+      return "Bakim Modu Aktif";
+    }
+    if(tool == "kapat"){
+      db.set(`bakim`,false);
+     nico.user.setActivity(ayar.durum)
+      return "bakim modu kapatildi";
+    }
+    }
+  }
+module.exports = greesy;
+                     
